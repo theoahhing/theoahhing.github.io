48f826d4eebb79af3d85552c848f2ed1aa88f022
theoahhing <theo.ahhing@outlook.com>
2026-03-17 13:09:43 +1000

    Added Slideshow.
    Added Slideshow container to cycle through photos of my travel shots every 7 seconds; added corresponding js code for transitioning function.

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.querySelector(".nav-links");
 
-menuBtn.addEventListener("click", () => {
+const images = [
+    "Photos/Travel Shots/IMG_5344.jpg",
+    "Photos/Travel Shots/IMG_5353.jpg",
+    "Photos/Travel Shots/IMG_7512.jpg",
+    "Photos/Travel Shots/IMG_7827.jpg",
+    "Photos/Travel Shots/IMG_7865.jpg",
+    "Photos/Travel Shots/SZJJ0705.JPG",
+    "Photos/Travel Shots/XPMY8925.JPG",
+    "Photos/Travel Shots/IMG_5250.JPG"
+];
 
-    navLinks.classList.toggle("show");
+let index = 0;
 
-});
+const slideshow = document.getElementById("slideshow-image");
+
+function changeImage() {
+
+    slideshow.style.opacity = 0;
+
+    setTimeout(() => {
+
+        index = (index + 1) % images.length;
+        slideshow.src = images[index];
+        slideshow.style.opacity = 1;
+
+    }, 700);
+
+}
+
+setInterval(changeImage, 5000);
